f0f85490298600bde1da971d215aa67cd3f8a496
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-24 02:57:12 +0900

    add: menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link"
+import Menu from "./Menu"
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+        <div className="h-full flex items-center justify-between">
+          <Link href="/">
+            <div className="text-2xl tracking-wide">LAMA</div>
+          </Link>
+          <Menu/>
+        </div>
+    </div>
   )
 }
 
