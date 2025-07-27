052994e8a53bd64f6de29f01d385203cc36a3cf0
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-27 16:41:45 +0900

    add: CategoryList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,14 @@ const HomePage = () => {
         <h1 className="text-2xl">Featured Products</h1>
         <ProductList/>
       </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <h1 className="text-2xl">Categories</h1>
+        <ProductList/>
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <ProductList/>
+      </div>
     </div>
   )
 }
