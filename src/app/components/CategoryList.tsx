9e586cfa8cfa4b6178587eaa0a17a3e5dd9d996e
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-27 21:11:25 +0900

    update: CategoryList

--- src/app/components/CategoryList.tsx
+++ src/app/components/CategoryList.tsx
@@ -1,17 +1,24 @@
+import { wixClientServer } from "../lib/wixClientServer";
 import Image from "next/image";
 import Link from "next/link";
 
-const CategoryList = () => {
+const CategoryList = async () => {
+  const wixClient = await wixClientServer();
+
+  const cats = await wixClient.collections.queryCollections().find();
+
   return (
     <div className="px-4 overflow-x-scroll scrollbar-hide">
       <div className="flex gap-4 md:gap-8">
+        {cats.items.map((item) => (
           <Link
-            href={`/list?cat=test`}
+            href={`/list?cat=${item.slug}`}
             className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+            key={item._id}
           >
             <div className="relative bg-slate-100 w-full h-96">
               <Image
-                src="https://images.pexels.com/photos/2076494/pexels-photo-2076494.jpeg"
+                src={item.media?.mainMedia?.image?.url || "/category.png"}
                 alt=""
                 fill
                 sizes="20vw"
@@ -19,9 +26,10 @@ const CategoryList = () => {
               />
             </div>
             <h1 className="mt-8 font-light text-xl tracking-wide">
-              Category Name
+              {item.name}
             </h1>
           </Link>
+        ))}
       </div>
     </div>
   );
