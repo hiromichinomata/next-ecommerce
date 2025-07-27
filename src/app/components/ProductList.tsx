baed27a71753a6d3b054db61ceea6a383df3b9be
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-27 19:05:08 +0900

    update: fetch data from wix api

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,67 +1,98 @@
+import { wixClientServer } from "../lib/wixClientServer";
+import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
+import Pagination from "./Pagination";
 
-const ProductList = () => {
+const PRODUCT_PER_PAGE = 8;
+
+const ProductList = async ({
+  categoryId,
+  limit,
+  searchParams,
+}: {
+  categoryId: string;
+  limit?: number;
+  searchParams?: any;
+}) => {
+  const wixClient = await wixClientServer();
+
+  const productQuery = wixClient.products
+    .queryProducts()
+    .startsWith("name", searchParams?.name || "")
+    .eq("collectionIds", categoryId)
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    );
+  // .find();
+
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
+
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      {res.items.map((product: products.Product) => (
         <Link
-          href={"/test"}
+          href={"/" + product.slug}
           className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+          key={product._id}
         >
           <div className="relative w-full h-80">
             <Image
-              src={"https://images.pexels.com/photos/2076494/pexels-photo-2076494.jpeg"}
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
               alt=""
               fill
               sizes="25vw"
               className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
             />
-            <Image
-            src={"https://images.pexels.com/photos/2076494/pexels-photo-2076494.jpeg"}
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-            />
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1]?.image?.url || "/product.png"}
+                alt=""
+                fill
+                sizes="25vw"
+                className="absolute object-cover rounded-md"
+              />
+            )}
           </div>
           <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$49</span>
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">${product.price?.price}</span>
           </div>
-          <div className="text-sm text-gray-500">My description</div>
+          {product.additionalInfoSections && (
+            <div></div>
+          )}
           <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
             Add to Cart
           </button>
         </Link>
-        <Link
-          href={"/test"}
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-        >
-          <div className="relative w-full h-80">
-            <Image
-              src={"https://images.pexels.com/photos/2076494/pexels-photo-2076494.jpeg"}
-              alt=""
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-            src={"https://images.pexels.com/photos/2076494/pexels-photo-2076494.jpeg"}
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$49</span>
-          </div>
-          <div className="text-sm text-gray-500">My description</div>
-          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
-            Add to Cart
-          </button>
-        </Link>
+      ))}
+      {searchParams?.cat || searchParams?.name ? (
+        <Pagination
+          currentPage={res.currentPage || 0}
+          hasPrev={res.hasPrev()}
+          hasNext={res.hasNext()}
+        />
+      ) : null}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,68 @@
-"use client"
+// "use client";
 
-import ProductList from "./components/ProductList"
-import Slider from "./components/Slider"
+import CategoryList from "./components/CategoryList";
+import ProductList from "./components/ProductList";
+import Skeleton from "./components/Skeleton";
+import Slider from "./components/Slider";
 
-const HomePage = () => {
+import { Suspense, useContext, useEffect } from "react";
+
+const HomePage = async () => {
+
+  // TEST (FETCHING ON THE CLIENT COMPONENT)
+
+  // const wixClient = useWixClient()
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+
+  //     console.log(res)
+  //   };
+
+  //   getProducts();
+  // }, [wixClient]);
+  
+
+  // TEST (FETCHING ON THE SERVER COMPONENT)
+
+  // const wixClient = await wixClientServer();
+
+  // const res = await wixClient.products.queryProducts().find();
+
+  // console.log(res);
+
   return (
-    <div className=''>
+    <div className="">
       <Slider />
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList/>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
-        <h1 className="text-2xl">Categories</h1>
-        <ProductList/>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <Suspense fallback={<Skeleton />}>
+          <CategoryList />
+        </Suspense>
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList/>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
