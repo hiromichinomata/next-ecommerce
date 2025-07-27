9219f81edb7343a5c169d45ca197799485e19a93
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-27 22:15:14 +0900

    update: list page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
 import Filter from "../components/Filter";
+import ProductList from "../components/ProductList";
+import Skeleton from "../components/Skeleton";
+import { wixClientServer } from "../lib/wixClientServer";
+import Image from "next/image";
+import { Suspense } from "react";
 
 const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
@@ -22,7 +32,15 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
+      <h1 className="mt-12 text-xl font-semibold">{cat?.collection?.name} For You!</h1>
+      <Suspense fallback={<Skeleton/>}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
   );
 };
