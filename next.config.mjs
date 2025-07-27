4de3a974af7e936a29709973c8e11b35c0049542
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-27 19:14:30 +0900

    fix: wixのimage urlを許可

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,20 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "people.pic1.co",
+      },
+      {
+        protocol: "https",
+        hostname: "app-uploads-cdn.fera.ai",
+      },
     ],
   },
 };
-export default nextConfig;
 
+export default nextConfig;
