7cc81e2b5da4c6b9f534fd7c0d495c914344477b
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-27 17:34:37 +0900

    update: slug page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,27 @@
-const SinglePage = () => {
-  return (
-    <div className=''>SinglePage</div>
-  )
-}
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import ProductImages from "../components/ProductImage";
 
-export default SinglePage
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+
+      {/* TEXTS */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">Product Description</p>
+        <div className="h-[2px] bg-gray-100" />
+        <h2 className="font-medium text-2xl">$11</h2>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="h-[2px] bg-gray-100" />
+        {/* REVIEWS */}
+        <h1 className="text-2xl">User Reviews</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
